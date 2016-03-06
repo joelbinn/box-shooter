edf0fcfda70dda3cc8399925d0dca251737501f2
Joel Binnquist <joel.binnquist@gmail.com>
2016-03-06 14:28:28 +0100

    First commit

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMover : MonoBehaviour {
+	
+	// degrees/s
+	public float spinSpeed = 180.0f; 
+	public float motionMagnitude = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		// Rotates around the up axis
+		gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+
+		// Move up and down over time
+		gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+	}
+}
